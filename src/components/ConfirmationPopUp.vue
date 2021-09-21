daed81d05e4fb1c67abd37e6f4f7d2c6a4a41ba1
Volodeccc <71349402+VolodecPHP@users.noreply.github.com>
2021-09-21 11:35:02 +0300

    Confirmation Popup

---
+++ src/components/ConfirmationPopUp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="confirmation-popup-wrapper">
+    <div class="card confirmation-popup">
+      <div class="confirmation-popup-title">
+        Ви впевнені, що ви хочете
+        <span id="confirmationPopupQuestion"></span>?
+      </div>
+      <button class="btn-primary" id="popupDecline">Відмінити</button>
+      <button class="btn-danger" id="popupConfirm">Продовжити</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
